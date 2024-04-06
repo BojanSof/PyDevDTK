b063e4f683a9fa428138da9904b1b9c0759398ae
Bojan Sofronievski <bojan.drago@gmail.com>
2024-04-06 12:50:32 +0200

    Add serial example

--- examples/serial_example.py
+++ examples/serial_example.py
@@ -2,6 +2,27 @@ import time
 
 from pydevdtk.coms.serial import Serial
 
+
+class Parser:
+    def __init__(self):
+        self.buffer = b""
+
+    def parse_data(self, data):
+        self.buffer += data
+        lines = self.buffer.split(b"\n")
+        self.buffer = lines.pop()
+
+        for line in lines:
+            line = line.strip()
+            if line:
+                values = line.decode("ascii").split(",")
+                try:
+                    parsed_values = tuple(map(float, values))
+                    yield parsed_values
+                except ValueError:
+                    pass
+
+
 ser = Serial()
 com_ports = ser.get_found_devices()
 print(f"Found ports: {com_ports}\n")
@@ -14,10 +35,9 @@ ser.open(port)
 
 t_run = 10
 t_start = time.time()
-sin = []
-cos = []
-data = bytearray()
+parser = Parser()
 while time.time() - t_start < t_run:
-    print(ser.get_data_event().decode("ascii"))
+    for sin, cos in parser.parse_data(ser.get_data_event()):
+        print(f"sin = {sin} | cos = {cos}")
 
 ser.close()
